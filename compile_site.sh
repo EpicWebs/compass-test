7f37a2c9df3e9be634b1ac1b39ce1b011e089d03
Rob Mehew <rob.mehew@weareknit.co.uk>
2014-08-05 14:44:54 +0100

    Updating SASS files

--- compile_site.sh
+++ compile_site.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # this pre-commit script uses compass to compile and compress stylesheet partials
-compass compile
+compass compile --output-style compressed --force
 echo "Compiling SASS";
 wait $!
 git add stylesheets/
