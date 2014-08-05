2643a4d099f50ac4e064548a5196a97c66bab619
Rob Mehew <rob.mehew@weareknit.co.uk>
2014-08-05 13:53:32 +0100

    Updating SASS

--- sass/recompile_sass.sh
+++ sass/recompile_sass.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
 # this pre-commit script uses compass to compile and compress stylesheet partials
+echo "Compiling SASS";
 compass compile sass/ -s compact --force --no-line-comments
 wait $!
 git add stylesheets/
