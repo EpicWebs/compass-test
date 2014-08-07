b1554306a3362cf4cde203d753dae19ced8c7f1d
Rob Mehew <rob.mehew@weareknit.co.uk>
2014-08-07 11:37:16 +0100

    Compress javascripts

--- compile_site.sh
+++ compile_site.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 #
 # this pre-commit script uses compass to compile and compress stylesheet partials
-echo "Checking javascript with jshint.";
+echo "/rChecking javascript with jshint.";
 grunt jshint
 
-echo "Making javascript Ugly.";
+echo "/rMaking javascript Ugly.";
 grunt uglify
 
-echo "Compiling SASS.";
+echo "/rCompiling SASS.";
 grunt sass
 
-echo "Adding compiled files to git.";
+echo "/rAdding compiled files to git.";
 wait $!
 git add --all
