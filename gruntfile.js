2cd8a8a5bc5f1225721224511a03d33b1220779d
Rob Mehew <rob.mehew@weareknit.co.uk>
2014-08-07 13:49:45 +0100

    Testing inline plugin

--- gruntfile.js
+++ gruntfile.js
@@ -1,53 +1,64 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    // Check the Javascript for errors
-    jshint: {
-        all: ['gruntfile.js', 'src/js/*.js']
-    },
-    // Minify and combine the Javascript
-    uglify: {
-        js: {
-            files: { 'build/js/scripts.js': ['src/js/script2.js', 'src/js/*.js'] },
-            options: {
-                preserveComments: false
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // Check the Javascript for errors
+        jshint: {
+            all: ['gruntfile.js', 'src/js/*.js']
+        },
+        // Minify and combine the Javascript
+        uglify: {
+            js: {
+                files: { 'build/js/scripts.js': ['src/js/script2.js', 'src/js/*.js'] },
+                options: {
+                    preserveComments: false
+                }
+            }
+        },
+        // Combine images from src/images/sprite/
+        // Create build/images/sprite.png
+        // Add the relevant CSS variables to src/sass/sprite.scss
+        sprite: {
+          all: {
+            src: 'src/images/sprite/*.png',
+            destImg: 'build/images/sprite.png',
+            destCSS: 'src/sass/sprite.scss'
+          }
+        },
+        // Compile the SASS and move to build folder
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'build/css/ie.css'    : 'src/sass/ie.scss',
+                    'build/css/print.css' : 'src/sass/print.scss',
+                    'build/css/screen.css': 'src/sass/screen.scss',
+                }
+            }
+        },
+        inline: {
+            dist: {
+                src: ['src/index.html'],
+                dest: ['index.html']
             }
         }
-    },
-    // Combine all the images from the sprite directory
-    // Add the relevant CSS variables to sprite.scss
-    sprite:{
-      all: {
-        src: 'src/images/sprite/*.png',
-        destImg: 'build/images/sprite.png',
-        destCSS: 'src/sass/sprite.scss'
-      }
-    },
-    sass: {
-        dist: {
-            options: {
-                style: 'compressed'
-            },
-            files: {
-                'build/css/ie.css'    : 'src/sass/ie.scss',
-                'build/css/print.css' : 'src/sass/print.scss',
-                'build/css/screen.css': 'src/sass/screen.scss',
-            }
-        }
-    }
-});
-    
-grunt.loadNpmTasks('grunt-contrib-jshint');
-grunt.registerTask('default', ['jshint']);
-    
-grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.registerTask('default', ['uglify']);
-    
-grunt.loadNpmTasks('grunt-spritesmith');
-grunt.registerTask('default', ['sprite']);
-    
-grunt.loadNpmTasks('grunt-contrib-sass');
-grunt.registerTask('default', ['sass']);
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.registerTask('default', ['jshint']);
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['uglify']);
+
+    grunt.loadNpmTasks('grunt-spritesmith');
+    grunt.registerTask('default', ['sprite']);
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.registerTask('default', ['sass']);
+
+    grunt.loadNpmTasks('grunt-inline');
+    grunt.registerTask('default', ['inline']);
 
 };
