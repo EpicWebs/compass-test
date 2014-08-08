74ecbfada9a22e02d3f72844464ae2c25c52e425
Rob Mehew <rob.mehew@weareknit.co.uk>
2014-08-08 17:27:23 +0100

    Deleted build files to test a full build from scratch.

--- compile_site.sh
+++ compile_site.sh
@@ -4,23 +4,18 @@
 echo -e "\nChecking javascript with jshint.";
 grunt jshint
 
-wait $!
 echo -e "\nMaking javascript ugly.";
 grunt uglify
 
-wait $!
 echo -e "\nCreating sprite.";
 grunt sprite
 
-wait $!
 echo -e "\nCompiling SASS.";
 grunt sass
 
-wait $!
 echo -e "\nMaking CSS inline.";
 grunt inline
 
-wait $!
 echo -e "\nRe-writing CSS links.";
 grunt processhtml
 
